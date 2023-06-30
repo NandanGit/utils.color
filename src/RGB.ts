eb18db1b3cc8d901b1f4fdc8036f221af1545b86
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-07-01 00:16:13 +0530

    Update RGB.ts
    Added toHSL method to RGBColor class

--- src/RGB.ts
+++ src/RGB.ts
@@ -1,3 +1,4 @@
+import { HSLColor } from './HSL';
 import { HexColor } from './Hex';
 
 export class RGBColor {
@@ -24,6 +25,48 @@ export class RGBColor {
       .padStart(2, '0');
 
     return new HexColor(`#${r}${g}${b}${a}`);
+  }
+
+  toHSL(): HSLColor {
+    const r = this.red / 255;
+    const g = this.green / 255;
+    const b = this.blue / 255;
+
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+
+    let h = (max + min) / 2;
+    let s = h;
+    const l = h;
+
+    if (max === min) {
+      h = s = 0; // achromatic
+    } else {
+      const d = max - min;
+
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+      switch (max) {
+        case r:
+          h = (g - b) / d + (g < b ? 6 : 0);
+          break;
+
+        case g:
+          h = (b - r) / d + 2;
+          break;
+
+        case b:
+          h = (r - g) / d + 4;
+          break;
+
+        default:
+          break;
+      }
+
+      h /= 6;
+    }
+
+    return new HSLColor(h * 360, s * 100, l * 100, this.alpha);
   }
 
   // Special methods
