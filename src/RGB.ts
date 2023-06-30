c55938deeef5b8dfbcce28bda99317650c41ef1a
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-07-01 00:13:44 +0530

    Update RGB.ts
    Added toHex method to RGBColor class

--- src/RGB.ts
+++ src/RGB.ts
@@ -1,3 +1,5 @@
+import { HexColor } from './Hex';
+
 export class RGBColor {
   constructor(
     readonly red: number,
@@ -10,6 +12,18 @@ export class RGBColor {
     this.green = green;
     this.blue = blue;
     this.alpha = alpha;
+  }
+
+  // Conversion methods
+  toHex(): HexColor {
+    const r = Math.round(this.red).toString(16).padStart(2, '0');
+    const g = Math.round(this.green).toString(16).padStart(2, '0');
+    const b = Math.round(this.blue).toString(16).padStart(2, '0');
+    const a = Math.round(this.alpha * 255)
+      .toString(16)
+      .padStart(2, '0');
+
+    return new HexColor(`#${r}${g}${b}${a}`);
   }
 
   // Special methods
