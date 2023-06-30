716e80e202f7cbba685bf8390261a8c5f16b9bc3
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-06-30 22:59:43 +0530

    Update HSL.ts
    Updated HSLColor to include alpha channel

--- src/HSL.ts
+++ src/HSL.ts
@@ -2,10 +2,12 @@ export class HSLColor {
   readonly hue: number;
   readonly saturation: number;
   readonly lightness: number;
+  readonly alpha: number;
 
-  constructor(hue: number, saturation: number, lightness: number) {
+  constructor(hue: number, saturation: number, lightness: number, alpha = 1) {
     this.hue = hue;
     this.saturation = saturation;
     this.lightness = lightness;
+    this.alpha = alpha;
   }
 }
