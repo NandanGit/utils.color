1a9bd8b04e95a0c386c57006b7f18f5d01ee6b93
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-06-30 23:57:03 +0530

    Update RGB.ts

--- src/RGB.ts
+++ src/RGB.ts
@@ -1,10 +1,11 @@
 export class RGBColor {
-  readonly red: number;
-  readonly green: number;
-  readonly blue: number;
-  readonly alpha: number;
-
-  constructor(red: number, green: number, blue: number, alpha = 1) {
+  constructor(
+    readonly red: number,
+    readonly green: number,
+    readonly blue: number,
+    readonly alpha = 1,
+    readonly bits = 8
+  ) {
     this.red = red;
     this.green = green;
     this.blue = blue;
