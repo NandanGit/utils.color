11484d838274398069b93f34cb6e0f1165a0a279
Nandan Reddy <nandan.reddy1133@gmail.com>
2023-06-30 23:00:33 +0530

    Update HSB.ts
    Updated HSBColor to include alpha clannal

--- src/HSB.ts
+++ src/HSB.ts
@@ -2,10 +2,12 @@ export class HSBColor {
   readonly hue: number;
   readonly saturation: number;
   readonly brightness: number;
+  readonly alpha: number;
 
-  constructor(hue: number, saturation: number, brightness: number) {
+  constructor(hue: number, saturation: number, brightness: number, alpha = 1) {
     this.hue = hue;
     this.saturation = saturation;
     this.brightness = brightness;
+    this.alpha = alpha;
   }
 }
